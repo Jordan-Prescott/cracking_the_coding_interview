34f0181ec5e25b957defe24a36e804969b389ed3
Jordan-Prescott <Jprescott23@gmail.com>
2023-08-25 17:02:41 +0100

    questions 1.1 & 1.2 added

---
+++ chapter_01/src/chapter_01/check_permutation_2.java
@@ -0,0 +1,18 @@
+package chapter_01;
+
+/**
+ * 
+ * Question.
+ * Given two strings, write a method to decide if one is permutation of the other.
+ * 
+ * @author jordanprescott
+ *
+ */
+public class check_permutation_2 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+}
---
+++ chapter_01/src/chapter_01/is_unique_1.java
@@ -0,0 +1,20 @@
+package chapter_01;
+
+/**
+ * 
+ * Question.
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What is you cannot use additional data structures?
+ * 
+ * @author jordanprescott
+ *
+ */
+
+public class is_unique_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
