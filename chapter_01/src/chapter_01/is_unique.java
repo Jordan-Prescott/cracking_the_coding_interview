53d0e9def0df58586ad3e7692c7ccf8a2e840cc8
Jordan-Prescott <Jprescott23@gmail.com>
2023-08-25 16:45:05 +0100

    start.

---
+++ chapter_01/src/chapter_01/is_unique.java
@@ -0,0 +1,10 @@
+package chapter_01;
+
+public class is_unique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ chapter_01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jordanprescott
+ *
+ */
+module chapter_01 {
+}
