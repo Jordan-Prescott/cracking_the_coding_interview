7eb1b0079fbb6ca9a284554365cf153158b758ab
Jordan-Prescott <Jprescott23@gmail.com>
2023-09-12 18:43:13 +0100

    questions added

--- chapter_01/src/chapter_01/rotate_matrix.java
+++ chapter_01/src/chapter_01/rotate_matrix.java
@@ -1,5 +1,13 @@
 package chapter_01;
 
+/**
+ * 
+ * Given an image represented by an NxN matrix, where each pixel in the image is 4 
+ * bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+ * 
+ * @author jordanprescott
+ *
+ */
 public class rotate_matrix {
 
 	public static void main(String[] args) {
