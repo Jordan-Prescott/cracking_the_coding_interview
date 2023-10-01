aa5c55297b6524cda323347b6a2907c8efeb97a6
Jordan-Prescott <Jprescott23@gmail.com>
2023-10-01 18:21:47 +0100

    delete_middle_node in-progress

--- chapter_02/src/chapter_02/delete_middle_node.java
+++ chapter_02/src/chapter_02/delete_middle_node.java
@@ -1,5 +1,7 @@
 package chapter_02;
 
+import java.util.LinkedList;
+
 /**
  * 
  * Implement an algorithm to delete a node in the middle (i.e., any node but
@@ -16,7 +18,39 @@ package chapter_02;
 public class delete_middle_node {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		LinkedList<Integer> linky_listy = new LinkedList<>();
+		
+		linky_listy.add(1);
+		linky_listy.add(2);
+		linky_listy.add(3);
+		linky_listy.add(10);
+		
+		linky_listy = middleNodeRemoved(linky_listy);
+		System.out.println(linky_listy);
+
+	}
+	
+	/**
+	 * 
+	 * This solution removes all middle nodes by looping the list starting with the 
+	 * first index up to but not including the last. This is achived by getting the size
+	 * of the list and minus one and each time an item is removed the counter is also
+	 * reduced by one to match the size of the list.
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public static LinkedList<Integer> middleNodeRemoved(LinkedList<Integer> list) {
+
+		for (int i = 1; i < list.size() - 1; i++) {
+			
+			list.remove(i);
+			i--;
+
+		}
+
+		return list;
 
 	}
 
