d97c01e71b2b58e589e5f1252ba50904e27c80b0
Jordan-Prescott <Jprescott23@gmail.com>
2023-11-07 16:09:22 +0000

    answered

--- chapter_01/src/chapter_01/palindrome_permutation.java
+++ chapter_01/src/chapter_01/palindrome_permutation.java
@@ -19,20 +19,22 @@ public class palindrome_permutation {
 
 	public static void main(String[] args) {
 		
-		isPalindromePermutation("Tact Coa"); // true
+		System.out.println(isPalindromePermutation("Tact Coa"));
+		System.out.println(isPalindromePermutation("Abba"));
+		System.out.println(isPalindromePermutation("Bernard"));
 
 	}
 	
 	public static boolean isPalindromePermutation(String my_string) {
 		
 		/**
-		 * I need run run through the string and add in chacter and int (number of times seen) to has
+		 * I need run through the string and add in chacter and int (number of times seen) to has
 		 * when I have the list there should be one odd numbered entry and the remaining should be even
 		 * if this is not true then return false.
 		 * 
 		 */
 		
-		Hashtable<Integer, Character> table = new Hashtable<>();
+		Hashtable<Character, Integer> table = new Hashtable<>();
 		
 		for (int i = 0; i < my_string.length(); i++) {
 			
@@ -40,16 +42,34 @@ public class palindrome_permutation {
 				continue;
 			}
 					
-			char toLowerCase = Character.toLowerCase(my_string.charAt(i));
-			table.put(i, toLowerCase);
+			char char_lower_case = Character.toLowerCase(my_string.charAt(i));
+			
+			if (!table.containsKey(char_lower_case)) {
+				table.put(char_lower_case, 1);
+			} else {
+				int table_entry = table.get(char_lower_case);
+				table.replace(char_lower_case, table_entry + 1);
+			}
 		}
 		
-		System.out.println(table);
+		int odd_total = 0;
+		
+		for (Character key : table.keySet()) {
+		    
+			int value = table.get(key);
+		    
+			if (!(value % 2 == 0)) {
+			   ++odd_total;
+			}
+			
+		}
+		
+		if (odd_total > 1) {
+			return false;
+		}
 		
 		return true;
 		
 	}
 	
-	
-
 }
